bc2dbcd26f6d064d8a817aa8df4e85f1f9591173
Destinee A. Adams <adestinee36@gmail.com>
2021-03-29 16:47:40 -0400

    impelmented the selling function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ password = ""
 My2factorApp = ""
 
 # get user name a password
-def getcredentials():
+def getCredentials():
     credentials = open("accountInfo.txt","r")
     username = credentials.readline()
     password = credentials.readline()
@@ -14,13 +14,33 @@ def getcredentials():
     credentials.close()
 
 # login in using two factor auth
-def logintoaccount():
+def loginToAccount():
     totp  = pyotp.TOTP(My2factorApp).now()
     login = r.login(username, password, mfa_code=totp)
 
-getcredentials()
-logintoaccount()
+#  get list of owned stocks and writes to file
+def getMyStocks():
+    my_stocks = r.build_holdings()
+    File_object = open("ownedStocks.txt","w")
 
+    # load file with stocks
+    for key in my_stocks.keys():
+        itemKey = key
+        itemValue = my_stocks[itemKey]
+        theitem = str(itemKey) + " " + str(itemValue)
+        File_object.write(theitem + "\n")
+    
+    File_object.close()
 
+def buyStock(symbol, quantity):
+    r.orders.order_buy_market(symbol, quantity)
 
+def sellStock(symbol, quantity):
+    r.orders.order_sell_market(symbol, quantity)
+
+getCredentials()
+loginToAccount()
+getMyStocks()
+# buyStock("BDRBF", 2)
+# sellStock("CPRX", 1)
 
